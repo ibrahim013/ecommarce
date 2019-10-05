4743701ebf08eefc3f637b543e4b69a4b3b71458
ibrahim013 <walaibrahim13@gmail.com>
2019-10-05 08:33:12 +0100

    sigin up functionality added

--- src/pages/authentication/authComponent.tsx
+++ src/pages/authentication/authComponent.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './authStyle.scss'
 
 import SignIn from '../../components/signin/siginComponent';
+import SiginUP from '../../components/sign-up/signUpComponent';
 
 const Authentication = () => {
 
   return(
     <div className='auth-wrapper'>
       <SignIn/>
+      <SiginUP/>
     </div>
   )
 }
